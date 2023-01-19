e2529e1b76f58746716977a42a2dcbe69a9ebd69
samliddell-sknups <90681478+samliddell-sknups@users.noreply.github.com>
2023-01-19 10:12:13 +0000

    PLATFORM-3392 Add separate DTO for retailer vs internal use (#5)

---
+++ src/dto/item-retailer.dto.ts
@@ -0,0 +1,89 @@
+import { ItemMediaDto } from './item-media.dto';
+import { ItemDto } from './item.dto';
+
+/**
+ * Extension of ItemDto to provide additional fields for retailer consumption.
+ * 
+ * This is intended to be exposed to retailers.
+ */
+export class RetailerItemDto extends ItemDto {
+
+  /**
+   * media links for the item
+   */
+  media: ItemMediaDto;
+
+}
+
+/**
+ * @deprecated
+ */
+export class LegacyRetailerItemDto extends RetailerItemDto {
+
+  /**
+   * item identifier
+   * @deprecated use token
+   */
+  thumbprint: string;
+
+  /**
+   * Host for flex server
+   * @deprecated should use media
+   */
+  flexHost: string;
+
+  /**
+   * Host for the sknups application
+   * @deprecated
+   */
+  sknappHost: string;
+
+  /**
+   * certificate version
+   * @deprecated
+   */
+  certVersion: string;
+
+  /**
+   * sale quantity 
+   * @deprecated use issue
+   */
+  saleQty: number;
+
+  /**
+   * claim code this item relates to, when created in a giveaway
+   * @deprecated use giveaway
+   */
+  claimCode: string | null;
+
+  /**
+   * Maximum quantity available
+   * @deprecated use maximum
+   */
+  maxQty: number;
+
+  /**
+   * Code representing the brand
+   * @deprecated use brand
+   */
+  brandCode: string;
+
+  /**
+   * code representing the platform
+   * @deprecated use platform
+   */
+  platformCode: string;
+
+  /**
+   * SKU of item
+   * @deprecated use sku
+   */
+  stockKeepingUnitCode: string;
+
+  /**
+   * The price for which this item was sold to the consumer.
+   * @deprecated use rrp
+   */
+  recommendedRetailPrice: number;
+
+}
