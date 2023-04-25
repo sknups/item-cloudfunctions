7694b6deaf20a037f816431a742e5b8882c5c3ca
Sam Liddell <90681478+samliddell-sknups@users.noreply.github.com>
2023-04-25 09:40:34 +0100

    PLATFORM-3783 delete deprecated item-create function (#41)

--- scripts/delete-tmp.sh
+++ scripts/delete-tmp.sh
@@ -2,7 +2,6 @@
 
 common_args="--project=drm-apps-01-43b0 --region=europe-west2 --quiet"
 
-gcloud functions delete item-create-tmp $common_args
 gcloud functions delete item-find-tmp $common_args
 gcloud functions delete item-get-tmp $common_args
 gcloud functions delete item-update-tmp $common_args
