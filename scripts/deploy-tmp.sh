c46a6f2f25f4ab14199952a8ca591577f65dde13
William J Ll Griffiths <604469+wgriffiths@users.noreply.github.com>
2023-04-25 17:04:17 +0100

    PLATFORM-3798 Disable build when deploying tmp functions (#42)

--- scripts/deploy-tmp.sh
+++ scripts/deploy-tmp.sh
@@ -9,6 +9,7 @@ common_args="${common_args} --security-level=secure-always"
 common_args="${common_args} --runtime=nodejs18"
 common_args="${common_args} --memory=512MB"
 common_args="${common_args} --set-env-vars CF_BASE_URL=https://europe-west2-drm-apps-01-43b0.cloudfunctions.net,ASSETS_URL=https://assets-dev.sknups.gg,FLEX_URL=https://flex-dev.sknups.com,SKNAPP_URL=https://app-dev.sknups.com"
+common_args="${common_args} --set-build-env-vars GOOGLE_NODE_RUN_SCRIPTS="
 
 npm run build
 
